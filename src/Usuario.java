e4539374264cbb6544f00dc73e2a482c08d8e49d
pabloacedo04 <pabloacedo2004@gmail.com>
2024-02-22 16:12:42 +0100

    Usuario terminado (Con comentarios)

--- src/Usuario.java
+++ src/Usuario.java
@@ -12,10 +12,12 @@ public class Usuario extends JFrame {
     private static DataInputStream dataInputStream;
     private static String nombre;
     public static void main(String[] args) throws IOException {
+        //Conexion con el servidor
         Socket socket = new Socket("localhost", 6000);
         dataOutputStream = new DataOutputStream(socket.getOutputStream());
         dataInputStream = new DataInputStream(socket.getInputStream());
 
+        //Comprobacion del nombre gracias al servidor
         nombre = JOptionPane.showInputDialog("Escribe tu nombre");
         dataOutputStream.writeUTF(nombre);
 
@@ -28,21 +30,22 @@ public class Usuario extends JFrame {
             recibido = dataInputStream.readUTF();
         }
 
+        //Creacion del usuario, ya con el nombre no repetido
         new Usuario(nombre);
 
-        new Thread(() -> {
-            try {
-                String recibo;
-                while (true) {
-                    recibo = dataInputStream.readUTF();
-                    chat.append(recibo + "\n");
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+        //Recibe mensajes del servidor hasta que se cierre el cliente y cada mensaje que recibe lo añade al area de chat
+        try {
+            String recibo;
+            while (true) {
+                recibo = dataInputStream.readUTF();
+                chat.append(recibo + "\n");
             }
-        }).start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    //Constructor del usuario (Interfaz gráfica)
     public Usuario(String name) {
         setTitle("Grupo ("+ name + ")");
         setSize(300, 400);
@@ -67,6 +70,7 @@ public class Usuario extends JFrame {
 
         setVisible(true);
     }
+    //Accion a realizar al pulsar el boton de enviar (Se envia el mensaje al servidor y se limpia la caja de texto donde se escriben los mensajes)
     private JButton getjButton() {
         JButton sendButton = new JButton("Enviar");
         sendButton.addActionListener(e -> {
